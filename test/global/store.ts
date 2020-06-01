4aa3d2d1a33761214383b0ebac3355386a06ef45
Michael Varrieur <michael.varrieur@boldpenguin.com>
2020-06-01 11:40:42 -0400

    Update unit test

--- test/global/store.ts
+++ test/global/store.ts
@@ -1,29 +1,26 @@
-import { createStore } from 'redux';
-import { Store } from '../../src/global/interfaces';
+import { createStore } from "redux";
+import { Store } from "../../src/global/interfaces";
 
 declare global {
   namespace NodeJS {
     interface Global {
       Context: {
         store: Store;
-      }
+      };
     }
   }
 }
 
 (global as any).Context = {};
-import '../../src/global/store';
+import initStore from "../../src/global/store";
+initStore();
 
-describe('@stencil/redux', () => {
-
-  describe('global/store', () => {
-
-    it('should return same redux store', () => {
+describe("@stencil/redux", () => {
+  describe("global/store", () => {
+    it("should return same redux store", () => {
       const store = createStore(() => {});
       global.Context.store.setStore(store);
       expect(global.Context.store.getStore()).toBe(store);
     });
-
   });
-
 });
